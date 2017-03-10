2232519a1e1ca5536ae4897226dbd05620c6d7eb
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-03-10 04:07:08 -0600

    we are xpe :)

--- xpe.mk
+++ xpe.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := xenonhd_lettuce
+PRODUCT_NAME := xpe_lettuce
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := lettuce
 
