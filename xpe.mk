7702f69e9819e5a90e517621c73428dd8e34f743
Rohan <drohan940@gmail.com>
2017-03-10 01:55:41 -0800

    Update and rename xenonhd.mk to xpe.mk

--- xpe.mk
+++ xpe.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/yu/lettuce/full_lettuce.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
